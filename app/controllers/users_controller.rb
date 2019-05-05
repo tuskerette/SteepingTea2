cd8582cb21330b7d1be59892e963d2a4e3c1b2f7
tuskerette <vgraffeo@everquote.com>
2019-05-05 12:47:00 -0400

    Add User show action and view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < Clearance::UsersController
 
-  private
+  def show
+    @user = User.find(params[:id])
+    @songs = @user.songs
+  end
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
