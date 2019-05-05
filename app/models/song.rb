93cb127574758394b6ba612ffff54f63bf342933
tuskerette <vgraffeo@everquote.com>
2019-05-05 11:44:16 -0400

    [Migration] Add duration to songs

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,4 +3,16 @@ class Song < ApplicationRecord
 
   validates :song_url, presence: true, format: URI::regexp(%w(http https))
   validates :user, presence: true
+
+  before_create :set_duration
+
+  scope :white,  -> { where("duration > ? and duration < ?", 30, 90)}
+  scope :green,  -> { where("duration > ? and duration < ?", 91, 180)}
+  scope :black,  -> { where("duration > ? and duration < ?", 181, 300) }
+  scope :herbal, -> { where("duration > ?", 301) }
+
+  def set_duration
+    video = Yt::Video.new url: self.song_url
+    self.duration = video.duration
+  end
 end
